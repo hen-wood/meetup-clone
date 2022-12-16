4e89cdf6fe40b9780da8988703883740c164c319
Henry W <henry.woodmansee@gmail.com>
2022-12-16 13:15:12 -1000

    added options to fn-ln migration file

--- backend/db/migrations/20221216174205-add-firstname-lastname.js
+++ backend/db/migrations/20221216174205-add-firstname-lastname.js
@@ -1,20 +1,36 @@
 "use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+	options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.addColumn("Users", "firstName", {
-			type: Sequelize.STRING,
-			allowNull: false
-		});
-		await queryInterface.addColumn("Users", "lastName", {
-			type: Sequelize.STRING,
-			allowNull: false
-		});
+		options.tableName = "Users";
+		await queryInterface.addColumn(
+			"Users",
+			"firstName",
+			{
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			options
+		);
+		await queryInterface.addColumn(
+			"Users",
+			"lastName",
+			{
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			options
+		);
 	},
 
 	async down(queryInterface, Sequelize) {
-		await queryInterface.removeColumn("Users", "firstName");
-		await queryInterface.removeColumn("Users", "lastName");
+		options.tableName = "Users";
+		await queryInterface.removeColumn("Users", "firstName", options);
+		await queryInterface.removeColumn("Users", "lastName", options);
 	}
 };
