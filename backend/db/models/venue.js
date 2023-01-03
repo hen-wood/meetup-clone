4bf637e7e90d001347d32927a0533cd22069b53b
Henry W <henry.woodmansee@gmail.com>
2023-01-02 19:07:28 -0800

    changed datatype from decimal to float on price, lat, lng properties in venue and event model/migration files

--- backend/db/models/venue.js
+++ backend/db/models/venue.js
@@ -44,11 +44,11 @@ module.exports = (sequelize, DataTypes) => {
 				allowNull: false
 			},
 			lat: {
-				type: DataTypes.DECIMAL,
+				type: DataTypes.FLOAT,
 				allowNull: false
 			},
 			lng: {
-				type: DataTypes.DECIMAL,
+				type: DataTypes.FLOAT,
 				allowNull: false
 			}
 		},
