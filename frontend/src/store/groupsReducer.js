0273e085c29252a123556df6cebcdf8cd5daa300
Henry W <henry.woodmansee@gmail.com>
2023-01-18 08:25:53 -0800

    added error handling on edit group, need to refine styling of errors

--- frontend/src/store/groupsReducer.js
+++ frontend/src/store/groupsReducer.js
@@ -1,19 +1,12 @@
 import { csrfFetch } from "./csrf";
 
 const GET_ALL_GROUPS = "groups/GET_ALL_GROUPS";
-
-const setGroups = allGroups => {
-	const allGroupsObj = {};
-	allGroups.forEach(group => {
-		allGroupsObj[group.id] = group;
-	});
-	return {
-		type: GET_ALL_GROUPS,
-		payload: allGroupsObj
-	};
-};
-
 const GET_USER_GROUPS = "groups/GET_USER_GROUPS";
+const GET_SINGLE_GROUP = "groups/GET_SINGLE_GROUP";
+const CREATE_GROUP = "groups/CREATE_GROUP";
+const UPDATE_GROUP = "groups/UPDATE_GROUP";
+const ADD_GROUP_IMAGE = "groupImages/ADD_GROUP_IMAGE";
+const REMOVE_GROUP = "groups/REMOVE_GROUP";
 
 const setUserGroups = userGroups => {
 	const userGroupsObj = {};
@@ -26,16 +19,12 @@ const setUserGroups = userGroups => {
 	};
 };
 
-const GET_SINGLE_GROUP = "groups/GET_SINGLE_GROUP";
-
 const setSingleGroup = singleGroup => {
 	return {
 		type: GET_SINGLE_GROUP,
 		payload: singleGroup
 	};
 };
-
-const CREATE_GROUP = "groups/CREATE_GROUP";
 
 const createGroup = newGroup => {
 	return {
@@ -44,12 +33,35 @@ const createGroup = newGroup => {
 	};
 };
 
-const UPDATE_GROUP = "groups/UPDATE_GROUP";
-
 const updateGroup = updatedGroup => {
 	return {
 		type: UPDATE_GROUP,
 		payload: updatedGroup
+	};
+};
+
+const addGroupImage = image => {
+	return {
+		type: ADD_GROUP_IMAGE,
+		payload: image
+	};
+};
+
+const removeGroup = groupId => {
+	return {
+		type: REMOVE_GROUP,
+		payload: groupId
+	};
+};
+
+const setGroups = allGroups => {
+	const allGroupsObj = {};
+	allGroups.forEach(group => {
+		allGroupsObj[group.id] = group;
+	});
+	return {
+		type: GET_ALL_GROUPS,
+		payload: allGroupsObj
 	};
 };
 
@@ -64,16 +76,7 @@ export const putGroup = (updatedGroup, groupId) => async dispatch => {
 
 	const data = await res.json();
 	dispatch(updateGroup(data));
-	return data;
-};
-
-const ADD_GROUP_IMAGE = "groupImages/ADD_GROUP_IMAGE";
-
-const addGroupImage = image => {
-	return {
-		type: ADD_GROUP_IMAGE,
-		payload: image
-	};
+	// return res;
 };
 
 export const postGroupImage = (image, groupId) => async dispatch => {
@@ -85,18 +88,9 @@ export const postGroupImage = (image, groupId) => async dispatch => {
 		}
 	});
 
-	const newImage = await response.json();
-	dispatch(addGroupImage(newImage));
-	return response;
-};
-
-const REMOVE_GROUP = "groups/REMOVE_GROUP";
-
-const removeGroup = groupId => {
-	return {
-		type: REMOVE_GROUP,
-		payload: groupId
-	};
+	const data = await response.json();
+	dispatch(addGroupImage(data));
+	return data;
 };
 
 export const getAllGroups = () => async dispatch => {
@@ -139,7 +133,7 @@ export const deleteGroup = groupId => async dispatch => {
 	});
 	const data = await response.json();
 	dispatch(removeGroup(groupId));
-	return response;
+	return data;
 };
 
 const initialState = {
@@ -175,13 +169,8 @@ export default function groupsReducer(state = initialState, action) {
 			return newState;
 		case UPDATE_GROUP:
 			newState = { ...state };
-			console.log(newState.allGroups[action.payload.id]);
-			// newState.allGroups[action.payload.id].name = action.payload.name;
-			// newState.allGroups[action.payload.id].about = action.payload.about;
-			// newState.allGroups[action.payload.id].type = action.payload.type;
-			// newState.allGroups[action.payload.id].private = action.payload.private;
-			// newState.allGroups[action.payload.id].city = action.payload.city;
-			// newState.allGroups[action.payload.id].state = action.payload.state;
+			const updatedGroupId = action.payload.id;
+			newState.allGroups[updatedGroupId] = { ...action.payload };
 			return newState;
 		default:
 			return state;
